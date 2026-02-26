564c9f01ad7270ef49f577b79489cb917e7ba098
Emmanuel O.O. <emmaigbagbo37@gmail.com>
2026-02-26 21:36:14 +0100

    Added new task

--- challenge3.js
+++ challenge3.js
@@ -2,5 +2,16 @@ let evenCount = 0;
 let oddCount = 0;
 
 for (let i = 1; i <= 40; i++) {
-    if (i % 2 === 0) 
+    if (i % 2 === 0) {
+        console.log(`${i} is even`);
+        evenCount++;
+    } else {
+        console.log(`${i} is odd`)
+        oddCount++;
+    }
 }
+
+// Final summary log
+console.log(`\nSummary:`);
+console.log(`Even numbers: ${evenCount}`);
+console.log(`Odd numbers: ${oddCount}`);
